d0188b3df3d73c4abc12cef8be5d971603d6485e
Aleksandr <74simakovan@gmail.com>
2022-06-10 12:05:55 +0500

    Задание №6

--- pythonProjectNew/DZ4/Задача 6.py
+++ pythonProjectNew/DZ4/Задача 6.py
@@ -1,10 +1,10 @@
 import math
 n = int(input())
-k = 1
+kf = 1
 s = 0
 s1 = 0
-for i in range(n):
-    s1 += 1/(k+1)
-    k += 1
-s = math.factorial(k)/s1
+for i in range(1, n+1):
+    kf *= i
+    s1 += 1 / (i+1)
+    s += kf/s1
 print('%.6f' %s)
