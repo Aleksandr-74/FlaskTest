a83aa7f8d7e711f92c03484acea91c1932631807
Aleksandr-74 <104676875+Aleksandr-74@users.noreply.github.com>
2022-06-10 12:17:56 +0500

    Delete Задание №1.py

--- pythonProjectNew/Задание №1.py
+++
@@ -1,8 +0,0 @@
-a, b, c = [float(x) for x in input().split()]
-
-if a == b and b == c:
-    print(3)
-elif a == b and b != c or a != b and b == c or a == c and c != b:
-    print(2)
-else:
-    print(0)
